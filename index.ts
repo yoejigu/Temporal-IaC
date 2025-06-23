8b11329c0bf5dc1ec536588b1777947e2efc6189
Yosef Ejigu <ytejigu@gmail.com>
2025-06-23 13:33:16 -0400

    stand up rdsdb

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as pulumi from "@pulumi/pulumi";
+import { RdsDatabase } from "./rdsComponent";
+
+const config = new pulumi.Config()
+let rdsAllocatedStorage = config.getNumber("rdsAllocatedStorage") || 20
+
+const rdsDb = new RdsDatabase("my-db", {
+    allocatedStorage: rdsAllocatedStorage,
+    maxAllocatedStorage: 0,
+    dbName: "testdb",
+    username: "yteDemoAdmin",
+    password: "t3stpwdforD3mo",
+    publiclyAccessible: true,
+    identifier: "temporaldemodb"
+});
+
+
+export const rdsAddress = rdsDb.RdsAddress; 
